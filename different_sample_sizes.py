99bbebc6a427dc6f1c8e2870b7a0bd39a23b9c3e
TY-Choi <tc2777@columbia.edu>
2017-11-06 14:09:22 -0500

    added samples of different sizes

---
+++ different_sample_sizes.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import random
+
+df = pd.read_csv('sampled_data1.csv')
+user_id = set(df['User'].values)
+batch1 = random.sample(user_id, 700)
+batch2 = random.sample(user_id, 1400)
+batch3 = random.sample(user_id, 2100)
+
+
+size700 = df[df['User'].isin(batch1)]
+size1400 = df[df['User'].isin(batch2)]
+size2100 = df[df['User'].isin(batch3)]
+
+print(len(set(size700['User'].values)))
+print(len(set(size1400['User'].values)))
+print(len(set(size2100['User'].values)))
+
+size700.to_csv("sample_700.csv")
+size1400.to_csv("sample_1400.csv")
+size2100.to_csv("sample_2100.csv")
