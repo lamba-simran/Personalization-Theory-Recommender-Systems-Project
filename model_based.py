27f8d659e36179596aa7af0ca5a0cd119d19443e
TY-Choi <tc2777@columbia.edu>
2017-11-07 14:37:02 -0500

    updated model_based

--- model_based.py
+++ model_based.py
@@ -40,14 +40,15 @@ def baseline_rmse(training, testing):
 
 
 def knnz_rmse(data, training, testing):
-    knn_param_grid = {'k': [20,30, 40], 'sim_options': {'name': ['msd', 'cosine', 'pearson'], 'min_support': [5,10],
-                                                        'user_based': [False]}}
+    knn_param_grid = {'k': [5, 10, 20], 'sim_options': {'name': ['msd', 'cosine', 'pearson'],
+                                                        'min_support': [1, 5],'user_based': [False]}}
 
     knnz_grid_search = GridSearch(KNNWithZScore, knn_param_grid, measures=['RMSE'], verbose=False)
     knnz_grid_search.evaluate(data)
     param = knnz_grid_search.best_params['RMSE']
     print('KNNWithZScore:', param)
-    knnz = KNNWithZScore(k=param['k'], name=param['sim_options']['name'], min_support=param['sim_options']['min_support'], user_based=param['sim_options']['user_based'] )
+    knnz = KNNWithZScore(k = param['k'], name=param['sim_options']['name'],
+                         min_support=param['sim_options']['min_support'], user_based=param['sim_options']['user_based'])
     knnz.train(training)
     predictions = knnz.test(testing)
     rmse = accuracy.rmse(predictions, verbose=True)
@@ -55,8 +56,8 @@ def knnz_rmse(data, training, testing):
 
 
 def knn_rmse(data, training, testing):
-    knn_param_grid = {'k': [20, 30, 40], 'sim_options': {'name': ['msd', 'cosine', 'pearson'], 'min_support': [5, 10],
-                                                         'user_based': [False]}}
+    knn_param_grid = {'k': [5, 10, 20], 'sim_options': {'name': ['msd', 'cosine', 'pearson'],
+                                                        'min_support': [1, 5], 'user_based': [False]}}
     knn_grid_search = GridSearch(KNNBasic, knn_param_grid, measures=['RMSE'], verbose=False)
     knn_grid_search.evaluate(data)
     param = knn_grid_search.best_params['RMSE']
@@ -69,8 +70,8 @@ def knn_rmse(data, training, testing):
 
 
 def knnm_rmse(data, training, testing):
-    knn_param_grid = {'k': [20,30, 40], 'sim_options': {'name': ['msd', 'cosine', 'pearson'], 'min_support': [5,10],
-                                                        'user_based': [False]}}
+    knn_param_grid = {'k': [5, 10, 20], 'sim_options': {'name': ['msd', 'cosine', 'pearson'],
+                                                        'min_support': [1, 5], 'user_based': [False]}}
     knnm_grid_search = GridSearch(KNNWithMeans, knn_param_grid, measures=['RMSE'], verbose=False)
     knnm_grid_search.evaluate(data)
     param = knnm_grid_search.best_params['RMSE']
@@ -95,31 +96,40 @@ def svd_rmse(data, training, testing):
     return rmse
 
 
+def main():
+    file_path = os.path.expanduser('sampled_data.csv')
+    reader = Reader(line_format='user item rating', sep=',')
+    data = Dataset.load_from_file(file_path, reader=reader)
+    data.split(n_folds=5)  # data can now be used normally
+    training = data.build_full_trainset()
+    testing = training.build_anti_testset()
+    svd_rmse = svd_rmse(data, training, testing)
+    svdpp_rmse = svdpp_rmse(data, training, testing)
+    nmf_rmse = nmf_rmse(data, training, testing)
+    baseline_rmse = baseline_rmse(training, testing)
+    knnz_rmse = knnz_rmse(data, training, testing)
+    knn_rmse = knn_rmse(data, training, testing)
+    knnm_rmse = knnm_rmse(data, training, testing)
 
-file_path = os.path.expanduser('sampled_data.csv')
-reader = Reader(line_format='user item rating', sep=',')
-data = Dataset.load_from_file(file_path, reader=reader)
-data.split(n_folds=5)  # data can now be used normally
-training = data.build_full_trainset()
-testing = training.build_anti_testset()
-svd_rmse = svd_rmse(data, training, testing)
-svdpp_rmse = svdpp_rmse(data, training, testing)
-nmf_rmse = nmf_rmse(data, training, testing)
-baseline_rmse = baseline_rmse(training, testing)
-knnz_rmse = knnz_rmse(data, training, testing)
-knn_rmse = knn_rmse(data, training, testing)
-knnm_rmse = knnm_rmse(data, training, testing)
+    objects = ('SVD', 'SVD++', 'NMF', 'Baseline', 'KNNWithZScore', 'KNNBasic', 'KNNWithMeans')
+    y_pos = np.arange(len(objects))
+    performance = [svd_rmse, svdpp_rmse, nmf_rmse, baseline_rmse, knnz_rmse, knn_rmse, knnm_rmse]
+    plt.bar(y_pos, performance, align='center', alpha=0.1)
+    plt.xticks(y_pos, objects)
+    plt.show()
 
-objects = ("SVD", "SVD++", "NMF", "Baseline", 'KNNWithZScore', 'KNNBasic', 'KNNWithMeans')
-y_pos = np.arange(len(objects))
-performance = [svd_rmse, svdpp_rmse, nmf_rmse, baseline_rmse, knnz_rmse, knn_rmse, knnm_rmse]
-plt.bar(y_pos, performance, align='center', alpha=0.1)
-plt.xticks(y_pos, objects)
-plt.show()
+if __name__ == "__main__":
+    main()
 
-# SVD: {'n_factors': 25, 'n_epochs': 40}
+# SVD: {'n_factors': 25, 'n_epochs': 50}
+# RMSE: 0.4666
 # SVDpp: {'n_factors': 25, 'n_epochs': 10}
+# RMSE: 0.2783
 # NMF: {'n_factors': 45, 'n_epochs': 50}
-# KNNWithZScore: {'k': 20, 'sim_options': {'name': 'cosine', 'min_support': 5, 'user_based': False}}
-# KNNBasic: {'k': 20, 'sim_options': {'name': 'msd', 'min_support': 10, 'user_based': False}}
-# KNNWithMeans: {'k': 20, 'sim_options': {'name': 'pearson', 'min_support': 5, 'user_based': False}}
+# RMSE: 1.2048
+# KNNWithZScore: {'k': 5, 'sim_options': {'name': 'cosine', 'min_support': 5, 'user_based': False}}
+# RMSE: 1.2176
+# KNNBasic: {'k': 5, 'sim_options': {'name': 'msd', 'min_support': 5, 'user_based': False}}
+# RMSE: 0.5045
+# KNNWithMeans: {'k': 5, 'sim_options': {'name': 'cosine', 'min_support': 5, 'user_based': False}}
+# RMSE: 1.2152
